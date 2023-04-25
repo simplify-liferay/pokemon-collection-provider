e69f407cdfe4974c89ae32a94a4e39b2599db967
pedroblandim <pedro.blandim@gmail.com>
2023-04-25 17:26:38 -0300

    adjust imports

--- src/main/java/com/simplifyliferay/pokemon/info/provider/PokemonInfoItemFieldSetProvider.java
+++ src/main/java/com/simplifyliferay/pokemon/info/provider/PokemonInfoItemFieldSetProvider.java
@@ -1,11 +1,7 @@
 package com.simplifyliferay.pokemon.info.provider;
 
-import com.liferay.info.field.InfoField;
 import com.liferay.info.field.InfoFieldSet;
-import com.liferay.info.field.type.NumberInfoFieldType;
-import com.liferay.info.field.type.TextInfoFieldType;
 import com.liferay.info.localized.InfoLocalizedValue;
-import com.simplifyliferay.pokemon.model.Pokemon;
 import org.osgi.service.component.annotations.Component;
 
 @Component(
